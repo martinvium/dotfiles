088bd4b5b45f0fcda3325f23020e4d6401d1a24e
Martin Vium <martin@codefuss.com>
2022-08-15 11:42:33 +0200

    Add zsh plugin file

---
+++ .zsh_plugins.sh
@@ -0,0 +1,2 @@
+source /Users/u0157541/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/u0157541/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
